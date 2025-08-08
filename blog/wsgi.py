6b86c17555b3b4845478ad5a295e1c73251855e9
gurjot <gurjotkaur358@gmail.com>
2025-08-08 07:11:06 +0530

    Added migrate and collectstatic commands on wsgi

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -13,4 +13,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
+# Run migrations and collectstatic once at startup
+from django.core.management import call_command
+try:
+    call_command('migrate')
+    call_command('collectstatic', interactive=False, verbosity=0)
+except Exception as e:
+    print("Startup command failed:", e)
+
 application = get_wsgi_application()
