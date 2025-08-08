50d0a6b3eea104f6b9c34e5a2a4d40af88fa6271
gurjot <gurjotkaur358@gmail.com>
2025-08-08 07:44:07 +0530

    Changed settings a lil bit

--- blog/settings.py
+++ blog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2jssxwp927%_nh1i)*qf29*s@x#d-nk)7j*hyxd@u8*lg@a=mk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
